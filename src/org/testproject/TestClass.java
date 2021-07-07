6b3222139ee5f8936f8ba5fd41105750102a9c5f
Kishorek1495 <kishorekriz111@gmail.com>
2021-07-07 14:48:39 +0530

    Jack commit

--- src/org/testproject/TestClass.java
+++ src/org/testproject/TestClass.java
@@ -3,10 +3,8 @@ package org.testproject;
 public class TestClass {
 	public static void testMethod() {
 		System.out.println("My test Method");
-		
-	}
+		}
 	public static void main(String[] args) {
 		testMethod();
 	}
-
 }
